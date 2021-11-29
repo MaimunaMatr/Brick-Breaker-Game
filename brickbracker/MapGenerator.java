c0c5385e2efdb2b06d68ff66dd4615cb09400498
MaimunaMatr <94953523+MaimunaMatr@users.noreply.github.com>
2021-11-29 12:34:23 +0300

    Update MapGenerator.java

--- brickbracker/MapGenerator.java
+++ brickbracker/MapGenerator.java
@@ -22,11 +22,11 @@ public class MapGenerator {
 for(int i = 0; i < map.length; i++) {
     for(int j = 0; j<map[0].length; j++) {
       if(map[i][j] > 0) {
-         g.setColor(Color.BLUE);
+         g.setColor(Color.BLACK);
          g.fillRect(j*brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 					
          g.setStroke(new BasicStroke(3));
-         g.setColor(Color.BLACK);
+         g.setColor(Color.PINK);
          g.drawRect(j*brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 	    }
 	  }
